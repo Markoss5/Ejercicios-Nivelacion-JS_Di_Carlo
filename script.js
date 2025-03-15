5d17e513e83f1a41e5ce66bd53eec4435463bd76
Markoss5 <48115731@est.ort.edu.ar>
2025-03-15 20:43:52 -0300

    version 2

--- script.js
+++ script.js
@@ -19,3 +19,50 @@ let multiplicacion = num1 * num2;
  console.log(resta);
  console.log(multiplicacion);
 }
+
+//Ejercicio 3
+
+function ComprobarNumero(){
+    let num = document.getElementById("numero").value;
+    if (num % 2 == 0){
+        console.log("es par");
+    }
+    else{
+        console.log("es impar");
+    }
+}
+
+//Ejercicio 4
+function ContarCaracteres(){
+    let PalabraUsuario = document.getElementById("PalabraUsuario").value;
+        console.log("El largo de la palabra es de " + PalabraUsuario.length + " caracteres ");
+}
+
+//Ejercicio 5
+
+function RepetirPalabra(){
+    let palabra = document.getElementById("Palabra").value;
+    let numeroRepeticion = document.getElementById("numero").value
+    numeroRepeticion = parseInt(numeroRepeticion);
+    for (let i = 0; i < numeroRepeticion; i++) {
+        console.log(palabra);
+    }
+}
+
+//Ejercicio 6
+
+function TablaMultiplicacion(){
+        
+let numm = document.getElementById("numm").value;
+const limite = 11;
+numm = parseInt(numm);
+    for (let i = 0 ; i < limite; i++) {
+        console.log(numm +" x " + i + " = " + numm*i);
+    }    
+}
+
+//Ejercicio 7 
+function adivinarNumero(){
+    let numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+    let adivinanza = parseInt(document.getElementById("adivinanza").value);
+}
